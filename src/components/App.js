73b4bfde0122748df53ef5240125bb82de2b9b09
github username <github email address>
2017-06-09 15:12:17 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,48 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.fetchFilters = this.fetchFilters.bind(this)
+    this.handleFilterChange = this.handleFilterChange.bind(this)
+    this.state = {
+      fruit: [],
+      currentFilter: null,
+      filters: []
+    }
+  }
+  componentDidMount(){
+    this.fetchFilters()
+    }
 
-export default App;
+    updatedFruitList() {
+      fetch('/api/fruit')
+        .then(response => response.json())
+        .then(fruits => this.setState({ fruit: fruits }));
+    }
+
+    fetchFilters = () => {
+        fetch('/api/fruit_types')
+          .then(response => response.json())
+          .then(filters => this.setState({ filters: filters }));
+      }
+
+      handleFilterChange = event => {
+        console.log('new filter: ', event.target.value);
+        this.setState({ selectedFilter: event.target.value });
+      }
+
+
+    render(){
+      return(
+        <div>
+          <FruitBasket filters={this.state.filters} currentFilter={this.state.currentFilter} fruit={this.state.fruit} updateFilterCallback={    this.updatedFruitList}/>
+        </div>
+      )
+    }
+  }
+
+
+//  <FilteredFruitList filter={this.state.currentFilter} fruit={this.state.fruit} />
+// <Filter filters={this.state.filters} currentFilter={this.state.currentFilter} handleChange={this.handleFilterChange} />
